0fca692a40341635646a6f947777fa0e8e75c170
Samir Husain <samir.husain26@gmail.com>
2026-05-09 19:58:04 -0400

    Fix Next.js SSR for client store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-import Americanizer from "@/components/Americanizer";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Americanizer = dynamic(() => import("@/components/Americanizer"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <Americanizer />;
